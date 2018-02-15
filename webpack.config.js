9b0068426a93d93ba96cf1c4124fa0cce224e61b
moay <mv@moay.de>
2018-02-15 09:48:40 +0100

    Added basic asset setup

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    // the project directory where compiled assets will be stored
+    .setOutputPath('public/build/')
+    // the public path used by the web server to access the previous directory
+    .setPublicPath('/build')
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+
+    // uncomment to define the assets of the project
+    // .addEntry('js/app', './assets/js/app.js')
+    // .addStyleEntry('css/app', './assets/css/app.scss')
+
+    .enableSassLoader()
+    .enableVueLoader()
+;
+
+module.exports = Encore.getWebpackConfig();

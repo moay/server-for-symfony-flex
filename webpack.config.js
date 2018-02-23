3e0cb7cf78a0b4ced3a6c4b51f82978e99a0a3f3
moay <mv@moay.de>
2018-02-23 10:15:36 +0100

    Added .gitattributes and config cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,14 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-    // the project directory where compiled assets will be stored
     .setOutputPath('public/build/')
-    // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(Encore.isProduction())
-
-    // uncomment to define the assets of the project
-    .addEntry('dashboard', './assets/js/dashboard.js')
-
     .enableVueLoader()
+
+    .addEntry('dashboard', './assets/js/dashboard.js')
 ;
 
 module.exports = Encore.getWebpackConfig();
